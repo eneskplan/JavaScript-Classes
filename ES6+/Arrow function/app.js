d8850975c9e7f10bf56d4659ba77e8ac33e64c7f
Enes Kaplan <enesskaplann02@gmail.com>
2025-12-23 16:14:18 +0300

    Arrow Function

---
+++ ES6+/Arrow function/app.js
@@ -0,0 +1,29 @@
+// const merhaba = function(){
+//     console.log("Merhaba");
+// }
+
+// merhaba();
+
+// Arrow Function 
+
+// const merhaba =() =>{
+//     console.log("Merhaba");
+// }
+
+// const merhaba = firstName =>{ // Tek parametre alacaksa parantez koymak zorunda değiliz.
+//     console.log("Merhaba",firstName);
+// }
+
+// merhaba("Enes");
+
+// Tek işlem yapılacaksa süslü paranteze gerek de yok 
+// const merhaba = firstName => console.log("Merhaba",firstName);
+
+// const cube = function(x){
+//     return x*x*x;
+// }
+
+// console.log(cube(4));
+
+// const cube = x => x*x*x; Tek işlem ve tek parametre varsa return yazmamıza bile gerek yok
+// console.log(cube(4));
