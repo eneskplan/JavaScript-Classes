fa1772e6199406f17ae1089f8d5ec178766c8880
Enes Kaplan <enesskaplann02@gmail.com>
2025-12-30 14:53:12 +0300

    post request

--- Ajax, Callback previous ES6/deneme/app.js
+++ Ajax, Callback previous ES6/deneme/app.js
@@ -1,25 +1,37 @@
 // https://jsonplaceholder.typicode.com/users/1/todos
 
-// GET Request
-// class Request {
-//   constructor() {
-//     this.xhr = new XMLHttpRequest();
-//   }
+class Request {
+  constructor() {
+    this.xhr = new XMLHttpRequest();
+  }
+  //   GET Request
+  get(url, callback) {
+    this.xhr.open("GET", url);
+    this.xhr.onload = () => {
+      if (this.xhr.status === 200) {
+        callback(null, this.xhr.responseText);
+      } else {
+        callback("Bir hata oluştu!", null);
+      }
+    };
+    this.xhr.send();
+  }
+  // POST Request
+  post(url, data, callback) {
+    this.xhr.open("POST", url);
+    this.xhr.setRequestHeader("Content-type", "application/json");
+    this.xhr.onload = () => {
+      if (this.xhr.status === 201) {
+        callback(null, this.xhr.responseText);
+      } else {
+        callback("herhangi bir hata oluştu", null);
+      }
+    };
+    this.xhr.send(JSON.stringify(data));
+  }
+}
 
-//   get(url, callback) {
-//     this.xhr.open("GET", url);
-//     this.xhr.onload = () => {
-//       if (this.xhr.status === 200) {
-//         callback(null, this.xhr.responseText);
-//       } else {
-//         callback("Bir hata oluştu!",null);
-//       }
-//     };
-//     this.xhr.send();
-//   }
-// }
-
-// const request = new Request();
+const request = new Request();
 // request.get(
 //   "https://jsonplaceholder.typicode.com/users/1/todos",
 //   function (err, response) {
@@ -31,3 +43,14 @@
 //   }
 // );
 
+request.post(
+  "https://jsonplaceholder.typicode.com/users/1/todos",
+  { userID: 2, title: "For Example" },
+  function (err, response) {
+    if (err === null) {
+      console.log(response);
+    } else {
+      console.log(err);
+    }
+  }
+);
