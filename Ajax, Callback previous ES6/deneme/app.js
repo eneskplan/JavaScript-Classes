46f08f18bad784c71b7e1d94d68b0893970d969a
Enes Kaplan <enesskaplann02@gmail.com>
2025-12-30 14:33:25 +0300

    get request

---
+++ Ajax, Callback previous ES6/deneme/app.js
@@ -0,0 +1,33 @@
+// https://jsonplaceholder.typicode.com/users/1/todos
+
+// GET Request
+// class Request {
+//   constructor() {
+//     this.xhr = new XMLHttpRequest();
+//   }
+
+//   get(url, callback) {
+//     this.xhr.open("GET", url);
+//     this.xhr.onload = () => {
+//       if (this.xhr.status === 200) {
+//         callback(null, this.xhr.responseText);
+//       } else {
+//         callback("Bir hata oluştu!",null);
+//       }
+//     };
+//     this.xhr.send();
+//   }
+// }
+
+// const request = new Request();
+// request.get(
+//   "https://jsonplaceholder.typicode.com/users/1/todos",
+//   function (err, response) {
+//     if (err === null) {
+//       console.log(response);
+//     } else {
+//       console.log(err);
+//     }
+//   }
+// );
+
