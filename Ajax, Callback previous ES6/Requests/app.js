dd7c6f09a6858eeded286b40a73e4382762be405
Enes Kaplan <enesskaplann02@gmail.com>
2025-12-30 14:55:42 +0300

    POST request

--- Ajax, Callback previous ES6/Requests/app.js
+++ Ajax, Callback previous ES6/Requests/app.js
@@ -1,28 +1,51 @@
-// Ajax, callback, http requests
+// https://jsonplaceholder.typicode.com/users/1/todos
 
 class Request {
   constructor() {
     this.xhr = new XMLHttpRequest();
   }
-
+  //   GET Request
   get(url, callback) {
     this.xhr.open("GET", url);
     this.xhr.onload = () => {
-        
       if (this.xhr.status === 200) {
         callback(null, this.xhr.responseText);
       } else {
-        callback("Hata oluştu", null);
+        callback("Bir hata oluştu!", null);
       }
     };
-
     this.xhr.send();
+  }
+  // POST Request
+  post(url, data, callback) {
+    this.xhr.open("POST", url);
+    this.xhr.setRequestHeader("Content-type", "application/json");
+    this.xhr.onload = () => {
+      if (this.xhr.status === 201) {
+        callback(null, this.xhr.responseText);
+      } else {
+        callback("herhangi bir hata oluştu", null);
+      }
+    };
+    this.xhr.send(JSON.stringify(data));
   }
 }
 
 const request = new Request();
-request.get(
-  "https://jsonplaceholder.typicode.com/users/1/posts",
+// request.get(
+//   "https://jsonplaceholder.typicode.com/users/1/todos",
+//   function (err, response) {
+//     if (err === null) {
+//       console.log(response);
+//     } else {
+//       console.log(err);
+//     }
+//   }
+// );
+
+request.post(
+  "https://jsonplaceholder.typicode.com/users/1/todos",
+  { userID: 2, title: "For Example" },
   function (err, response) {
     if (err === null) {
       console.log(response);
