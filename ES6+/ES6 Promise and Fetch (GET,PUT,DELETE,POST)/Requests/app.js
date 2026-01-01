ce5c710a58ae542fca08389477042d03403ba7d9
Enes Kaplan <enesskaplann02@gmail.com>
2026-01-02 01:04:19 +0300

    POST Request

--- ES6+/ES6 Promise and Fetch (GET,PUT,DELETE,POST)/Requests/app.js
+++ ES6+/ES6 Promise and Fetch (GET,PUT,DELETE,POST)/Requests/app.js
@@ -7,11 +7,33 @@ class Request {
         .catch((err) => reject(err));
     });
   }
+
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+      })
+        .then((response) => response.json())
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    });
+  }
 }
 
 const request = new Request();
-let albums;
-request
-  .get("https://jsonplaceholder.typicode.com/albums")
-  .then((albums) => console.log(typeof albums))
-  .catch((err) => console.log(err));
+// let albums;
+// request
+//   .get("https://jsonplaceholder.typicode.com/albums")
+//   .then((albums) => console.log(albums))
+//   .catch((err) => console.log(err));
+
+request.post("https://jsonplaceholder.typicode.com/albums", {
+  userId: 1,
+  title: "Thriller",
+})
+.then(newAlbums => {
+  console.log(newAlbums)
+})
+.catch(err => console.log(err))
