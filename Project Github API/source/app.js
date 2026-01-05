5ea9527becf72891d924e08955891c9e77207916
Enes Kaplan <enesskaplann02@gmail.com>
2026-01-05 14:04:05 +0300

    js files

---
+++ Project Github API/source/app.js
@@ -0,0 +1,44 @@
+// Select Elements
+
+const githubForm = document.getElementById("github-form");
+const nameInput = document.getElementById("githubname");
+const clearLastUsers = document.getElementById("clear-last-users");
+const lastUsers = document.getElementById("last-users");
+const github = new Github(); // Github object
+const ui = new UI();
+eventListeners();
+
+function eventListeners() {
+  githubForm.addEventListener("submit", getData);
+  clearLastUsers.addEventListener("click", clearAllSearched);
+  document.addEventListener("DOMContentLoaded", getAllSearched);
+}
+
+function getData(e) {
+  // Tüm arananları temizle
+  let username = nameInput.value.trim();
+  if (username === "") {
+    alert("Lütfen geçerli bir kullanıcı adı giriniz !");
+  } else {
+    github
+      .getGithubData(username)
+      .then((response) => {
+        if (response.user.message === "Not Found") {
+          // Hata mesajı
+          console.log("hata")
+        } 
+        else {
+            ui.showUserInfo(response.user)
+        }
+      })
+      .catch((err) => console.log(err));
+  }
+  ui.clearInput();
+  e.preventDefault();
+}
+
+function clearAllSearched() {
+  // Arananları storagedan al ve UI'ye ekle
+}
+
+function getAllSearched() {}
