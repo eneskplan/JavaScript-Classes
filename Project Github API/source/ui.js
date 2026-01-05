a5dad4c130b3e3bbc8f3e60744e07aef9e5d3aa9
Enes Kaplan <enesskaplann02@gmail.com>
2026-01-05 15:32:18 +0300

    addSearchedUserToUI

--- Project Github API/source/ui.js
+++ Project Github API/source/ui.js
@@ -1,18 +1,18 @@
 class UI {
-    constructor(){
-        this.profileDiv = document.getElementById("profile")
-        this.repoDiv = document.getElementById("repos")
-        this.lastUsers = document.getElementById("last-users")
-        this.inputField = document.getElementById("githubname")
-        this.cardBody = document.querySelector(".card-body")
-    }
+  constructor() {
+    this.profileDiv = document.getElementById("profile");
+    this.repoDiv = document.getElementById("repos");
+    this.lastUsers = document.getElementById("last-users");
+    this.inputField = document.getElementById("githubname");
+    this.cardBody = document.querySelector(".card-body");
+  }
 
-    clearInput(){
-        this.inputField.value="";
-    }
+  clearInput() {
+    this.inputField.value = "";
+  }
 
-    showUserInfo(user){
-        this.profileDiv.innerHTML = `<div class="card card-body mb-3">
+  showUserInfo(user) {
+    this.profileDiv.innerHTML = `<div class="card card-body mb-3">
                     <div class="row">
                       <div class="col-md-4">
                         <a href="${user.html_url}" target = "_blank">
@@ -51,13 +51,13 @@ class UI {
                                
                             
                       </div>
-                </div>`
-    }
+                </div>`;
+  }
 
-    showRepoInfo(repos){
-        this.repoDiv.innerHTML = "";
-        repos.forEach(repo=>{
-            this.repoDiv.innerHTML +=`
+  showRepoInfo(repos) {
+    this.repoDiv.innerHTML = "";
+    repos.forEach((repo) => {
+      this.repoDiv.innerHTML += `
             <div class="mb-2 card-body">
                     <div class="row">
                         <div class="col-md-2">
@@ -75,18 +75,29 @@ class UI {
                         </div>
                 </div>
 
-                </div>`
-        })
-    }
+                </div>`;
+    });
+  }
 
-    showError(message){
-        const div = document.createElement("div");
-        div.className = "alert alert-danger";
-        div.textContent = message;
+  showError(message) {
+    const div = document.createElement("div");
+    div.className = "alert alert-danger";
+    div.textContent = message;
 
-        this.cardBody.appendChild(div);
-        setTimeout(()=>{
-            div.remove()
-        },2000)
+    this.cardBody.appendChild(div);
+    setTimeout(() => {
+      div.remove();
+    }, 2000);
+  }
+
+  addSearchedUserToUI(username) {
+    let users = Storage.getSearchedUsersFromStorage();
+    if (users.indexOf(username) === -1) {
+      //<li class="list-group-item">asdaskdjkasjkşdjşasjd</li>
+      const li = document.createElement("li");
+      li.className = "list-group-item";
+      li.textContent = username;
+      this.lastUsers.appendChild(li);
     }
+  }
 }
